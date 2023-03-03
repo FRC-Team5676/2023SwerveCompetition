920168992d135310bcd2f5697f4b9e1d53d0c02a
Eric Macy <eric_macy@nonik.com>
2023-03-03 12:02:25 -0500

    drive change

--- src/main/java/frc/robot/commands/auto/AutoRoutines.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutines.java
@@ -9,32 +9,43 @@ import frc.robot.subsystems.LowerArmSubsystem;
 import frc.robot.subsystems.UpperArmSubsystem;
 
 public class AutoRoutines {
-    public static Command PlaceConeAndLeave(LowerArmSubsystem lowerArm, UpperArmSubsystem upperArm, IntakeSubsystem intake, DriveSubsystem robot) {
-        return Commands.sequence(
-                Commands.parallel(
-                new StartEndCommand(() -> robot.drive(0, 0, 0, true), () -> robot.drive(0, 0, 0, true), robot)
-                        .withTimeout(0.5),
-                new StartEndCommand(() -> upperArm.moveToPosition(40), () -> upperArm.driveArm(0), upperArm)
-                        .withTimeout(2),
-                new StartEndCommand(() -> lowerArm.moveToPosition(-20), () -> lowerArm.driveArm(0), lowerArm)
-                        .withTimeout(2)),
-                new StartEndCommand(() -> upperArm.moveToPosition(163), () -> upperArm.driveArm(0), upperArm)
-                        .withTimeout(3),
-                new StartEndCommand(() -> lowerArm.moveToPosition(87), () -> lowerArm.driveArm(0), lowerArm)
-                        .withTimeout(3),
-                Commands.parallel(
-                new StartEndCommand(() -> upperArm.moveToPosition(145), () -> upperArm.driveArm(0), upperArm)
-                        .withTimeout(2),
-                new StartEndCommand(() -> intake.moveIntakeToPosition(10), () -> intake.rotateIntake(0), intake)
-                        .withTimeout(2)),
-                new StartEndCommand(() -> robot.drive(0.3, 0, 0, true), () -> robot.drive(0, 0, 0, true), robot)
-                        .withTimeout(1),
-                new StartEndCommand(() -> upperArm.moveToPosition(0), () -> upperArm.driveArm(0), upperArm)
-                        .withTimeout(3),
-                Commands.parallel(
-                new StartEndCommand(() -> lowerArm.moveToPosition(22), () -> lowerArm.driveArm(0), lowerArm)
-                        .withTimeout(3),
-                new StartEndCommand(() -> robot.drive(0.4, 0, 0, true), () -> robot.drive(0, 0, 0, true), robot)
-                        .withTimeout(1)));
-    }
+        public static Command PlaceConeAndLeave(LowerArmSubsystem lowerArm, UpperArmSubsystem upperArm,
+                        IntakeSubsystem intake, DriveSubsystem robot) {
+                return Commands.sequence(
+                                Commands.parallel(
+                                                new StartEndCommand(() -> robot.drive(0, 0, 0, true),
+                                                                () -> robot.drive(0, 0, 0, true), robot)
+                                                                .withTimeout(2),
+                                                new StartEndCommand(() -> upperArm.moveToPosition(40),
+                                                                () -> upperArm.driveArm(0), upperArm)
+                                                                .withTimeout(2),
+                                                new StartEndCommand(() -> lowerArm.moveToPosition(-20),
+                                                                () -> lowerArm.driveArm(0), lowerArm)
+                                                                .withTimeout(2)),
+                                new StartEndCommand(() -> upperArm.moveToPosition(163), () -> upperArm.driveArm(0),
+                                                upperArm)
+                                                .withTimeout(3),
+                                new StartEndCommand(() -> lowerArm.moveToPosition(87), () -> lowerArm.driveArm(0),
+                                                lowerArm)
+                                                .withTimeout(3),
+                                Commands.parallel(
+                                                new StartEndCommand(() -> upperArm.moveToPosition(145),
+                                                                () -> upperArm.driveArm(0), upperArm)
+                                                                .withTimeout(2),
+                                                new StartEndCommand(() -> intake.moveIntakeToPosition(10),
+                                                                () -> intake.rotateIntake(0), intake)
+                                                                .withTimeout(2)),
+                                new StartEndCommand(() -> robot.drive(0.3, 0, 0, true),
+                                                () -> robot.drive(0, 0, 0, true), robot)
+                                                .withTimeout(1),
+                                new StartEndCommand(() -> upperArm.moveToPosition(0), () -> upperArm.driveArm(0),
+                                                upperArm)
+                                                .withTimeout(3),
+                                new StartEndCommand(() -> robot.drive(0.3, 0, 0, true),
+                                                () -> robot.drive(0, 0, 0, true), robot)
+                                                .withTimeout(1),
+                                new StartEndCommand(() -> lowerArm.moveToPosition(22),
+                                                () -> lowerArm.driveArm(0), lowerArm)
+                                                .withTimeout(3));
+        }
 }
