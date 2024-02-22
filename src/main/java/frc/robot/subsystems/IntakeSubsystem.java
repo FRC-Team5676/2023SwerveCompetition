ea6c2c083b0a029dc223f9efcc59cfa4c52ed3b9
Team5676 <team5676@outlook.com>
2024-02-22 18:14:41 -0500

    Cameras working and adjusted shooter speed

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -12,33 +12,37 @@ public class IntakeSubsystem extends SubsystemBase {
 
   public double rotations;
 
-  private final int m_intakeArmCanId = 30;
-  private final boolean m_intakeArmMotorReversed = true;
+  private final int m_leftintakeArmCanId = 21;
+  private final boolean m_leftintakeArmMotorReversed = true;
 
-  private final RelativeEncoder m_driveEncoder;
-  private final CANSparkMax m_driveMotor;
-  private final SparkMaxPIDController m_driveController;
+  private final RelativeEncoder m_leftdriveEncoder;
+  private final CANSparkMax m_leftdriveMotor;
+  private final SparkMaxPIDController m_leftdriveController;
 
+  
   private final double minRotations = 0;
   private final double maxRotations = 9.23;
 
   public IntakeSubsystem() {
     // Drive Motor setup
-    m_driveMotor = new CANSparkMax(m_intakeArmCanId, MotorType.kBrushless);
-    m_driveMotor.restoreFactoryDefaults();
-    m_driveMotor.setInverted(m_intakeArmMotorReversed);
-    m_driveMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    m_leftdriveMotor = new CANSparkMax(m_leftintakeArmCanId, MotorType.kBrushless);
+    m_leftdriveMotor.restoreFactoryDefaults();
+    m_leftdriveMotor.setInverted(m_leftintakeArmMotorReversed);
+    m_leftdriveMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
+    
     // drive encoder setup
-    m_driveEncoder = m_driveMotor.getEncoder();
+    m_leftdriveEncoder = m_leftdriveMotor.getEncoder();
 
-    m_driveController = m_driveMotor.getPIDController();
-    m_driveController.setP(0.01);
-    m_driveController.setI(0);
-    m_driveController.setD(0);
-    m_driveController.setIZone(0);
-    m_driveController.setFF(0);
-    m_driveController.setOutputRange(-1, 1);
+    m_leftdriveController = m_leftdriveMotor.getPIDController();
+    m_leftdriveController.setP(0.01);
+    m_leftdriveController.setI(0);
+    m_leftdriveController.setD(0);
+    m_leftdriveController.setIZone(0);
+    m_leftdriveController.setFF(0);
+    m_leftdriveController.setOutputRange(-1, 1);
+
+   
 
     ShuffleboardContent.initIntakeArm(this);
   }
@@ -57,7 +61,7 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   public double getPosition() {
-    return m_driveEncoder.getPosition();
+    return m_leftdriveEncoder.getPosition();
   }
 
   public void setIntakePosition(double position) {
@@ -65,17 +69,18 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   public void rotateIntake(double throttle) {
-    m_driveMotor.set(throttle);
+    m_leftdriveMotor.set(throttle);
   }
 
   public void stop() {
-    m_driveMotor.set(0);
+    m_leftdriveMotor.set(0);
   }
 
   public void setReferencePeriodic() {
-    m_driveController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+    m_leftdriveController.setReference(rotations, CANSparkMax.ControlType.kPosition);
   }
 
+  
   public void setReferenceValue(double rotation) {
     rotations = rotation;
   }
